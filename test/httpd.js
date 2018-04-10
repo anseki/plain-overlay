a7d7b2ebbd0fb19a10b609973c92acee5d2fcd21
anseki <AnSeki.aff@gmail.com>
2018-04-10 16:05:55 +0900

    Fix: nested `node_module` is not checked

--- test/httpd.js
+++ test/httpd.js
@@ -6,13 +6,6 @@ const
   DOC_ROOT = __dirname,
   PORT = 8080,
 
-  http = require('http'),
-  staticAlias = require('node-static-alias'),
-  log4js = require('log4js'),
-  filelist = require('stats-filelist'),
-  path = require('path'),
-  fs = require('fs'),
-
   MODULE_PACKAGES = [
     'jasmine-core',
     'test-page-loader',
@@ -20,21 +13,30 @@ const
     'm-class-list'
   ],
 
-  EXT_DIR = path.resolve(__dirname, '../../test-ext');
+  http = require('http'),
+  staticAlias = require('node-static-alias'),
+  logger = (() => {
+    const log4js = require('log4js');
+    log4js.configure({
+      appenders: {
+        out: {
+          type: 'console',
+          layout: {
+            type: 'pattern',
+            pattern: '%[[%r]%] %m' // Super simple format
+          }
+        }
+      },
+      categories: {default: {appenders: ['out'], level: 'info'}}
+    });
+    return log4js.getLogger('node-static-alias');
+  })(),
 
-log4js.configure({
-  appenders: {
-    out: {
-      type: 'console',
-      layout: {
-        type: 'pattern',
-        pattern: '%[[%r]%] %m' // Super simple format
-      }
-    }
-  },
-  categories: {default: {appenders: ['out'], level: 'info'}}
-});
-let logger = log4js.getLogger('node-static-alias');
+  filelist = require('stats-filelist'),
+  path = require('path'),
+  fs = require('fs'),
+
+  EXT_DIR = path.resolve(__dirname, '../../test-ext');
 
 http.createServer((request, response) => {
   request.addListener('end', () => {
@@ -45,6 +47,7 @@ http.createServer((request, response) => {
         { // node_modules
           match: new RegExp(`^/${packageName}/.+`),
           serve: `${require.resolve(packageName).replace(
+            // Include `packageName` for nested `node_modules`
             new RegExp(`^(.*[/\\\\]node_modules)[/\\\\]${packageName}[/\\\\].*$`), '$1')}<% reqPath %>`,
           allowOutside: true
         })).concat([
@@ -101,19 +104,19 @@ http.createServer((request, response) => {
             allowOutside: true
           }
         ]),
-      logger: logger
+      logger
     }))
-    .serve(request, response, e => {
-      if (e) {
-        response.writeHead(e.status, e.headers);
-        logger.error('(%s) %s', request.url, response.statusCode);
-        if (e.status === 404) {
-          response.end('Not Found');
+      .serve(request, response, e => {
+        if (e) {
+          response.writeHead(e.status, e.headers);
+          logger.error('(%s) %s', request.url, response.statusCode);
+          if (e.status === 404) {
+            response.end('Not Found');
+          }
+        } else {
+          logger.info('(%s) %s', request.url, response.statusCode);
         }
-      } else {
-        logger.info('(%s) %s', request.url, response.statusCode);
-      }
-    });
+      });
   }).resume();
 }).listen(PORT);
 
