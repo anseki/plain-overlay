70ffd2185c8c8414b16a4fe4188bb4283f1becbd
anseki <AnSeki.aff@gmail.com>
2018-06-16 18:58:06 +0900

    Update dependencies

--- test/plain-overlay-limit.js
+++ test/plain-overlay-limit.js
@@ -709,10 +709,11 @@ function abort(props) {
 
 /**
  * @param {props} props - `props` of instance.
- * @param {boolean} [force] - Skip transition.
+ * @param {boolean} force - Skip transition.
+ * @param {Array} args - Arguments that are passed to procToOn.
  * @returns {void}
  */
-function _on(props, force) {
+function _on(props, force, args) {
   if (props.isOn && props.state === STATE_STOPPED || props.isOn && props.state !== STATE_STOPPED && !force) {
     return;
   }
@@ -723,7 +724,8 @@ function _on(props, force) {
   */
 
   if (props.options.procToOn) {
-    props.options.procToOn.call(props.ins, !!force);
+    args.unshift(!!force);
+    props.options.procToOn.apply(props.ins, args);
   }
 
   if (force || !props.isOn && props.state === STATE_DELAYING || -props.delay > props.duration) {
@@ -757,10 +759,11 @@ function _on(props, force) {
 
 /**
  * @param {props} props - `props` of instance.
- * @param {boolean} [force] - Skip transition.
+ * @param {boolean} force - Skip transition.
+ * @param {Array} args - Arguments that are passed to procToOff.
  * @returns {void}
  */
-function _off(props, force) {
+function _off(props, force, args) {
   if (!props.isOn && props.state === STATE_STOPPED || !props.isOn && props.state !== STATE_STOPPED && !force) {
     return;
   }
@@ -771,7 +774,8 @@ function _off(props, force) {
   */
 
   if (props.options.procToOff) {
-    props.options.procToOff.call(props.ins, !!force);
+    args.unshift(!!force);
+    props.options.procToOff.apply(props.ins, args);
   }
 
   if (force || props.isOn && props.state === STATE_DELAYING || -props.delay > props.duration) {
@@ -908,13 +912,20 @@ var TimedTransition = function () {
     finishAll(props);
   }
 
-  /**
-   * @param {Object} options - New options.
-   * @returns {TimedTransition} Current instance itself.
-   */
-
-
   _createClass(TimedTransition, [{
+    key: 'remove',
+    value: function remove() {
+      var props = insProps[this._id];
+      clearTimeout(props.timer);
+      delete insProps[this._id];
+    }
+
+    /**
+     * @param {Object} options - New options.
+     * @returns {TimedTransition} Current instance itself.
+     */
+
+  }, {
     key: 'setOptions',
     value: function setOptions(options) {
       if (isObject(options)) {
@@ -927,6 +938,7 @@ var TimedTransition = function () {
      * Set `on`.
      * @param {boolean} [force] - Set `on` it immediately without transition.
      * @param {Object} [options] - New options.
+     * @param {...{}} [args] - Arguments that are passed to procToOn.
      * @returns {TimedTransition} Current instance itself.
      */
 
@@ -939,20 +951,28 @@ var TimedTransition = function () {
       }
 
       this.setOptions(options);
-      _on(insProps[this._id], force);
+      _on(insProps[this._id], force, Array.prototype.slice.call(arguments, 2));
       return this;
     }
 
     /**
      * Set 'off'.
      * @param {boolean} [force] - Set `off` it immediately without transition.
+     * @param {Object} [options] - New options.
+     * @param {...{}} [args] - Arguments that are passed to procToOff.
      * @returns {TimedTransition} Current instance itself.
      */
 
   }, {
     key: 'off',
-    value: function off(force) {
-      _off(insProps[this._id], force);
+    value: function off(force, options) {
+      if (arguments.length < 2 && typeof force !== 'boolean') {
+        options = force;
+        force = false;
+      }
+
+      this.setOptions(options);
+      _off(insProps[this._id], force, Array.prototype.slice.call(arguments, 2));
       return this;
     }
   }, {
